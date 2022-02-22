b8671f4b1b9efc57096606c743d9a3aed7802e9d
Coops023 <bjorkc27@gmail.com>
2022-02-22 22:13:28 +0100

    added redux

--- server/routes/index.js
+++ server/routes/index.js
@@ -11,34 +11,34 @@ router.get("/", (req, res, next) => {
 // get all products
 router.get("/all-products", async (req, res) => {
   try {
-    const products = await Product.find().limit(10);
-    res.json({ products });
-    console.log(`Found ${products.length} products`);
+    const item = await Product.find().limit(10);
+    res.json({ item });
+    console.log(`Found ${item.length} products`);
   } catch (err) {}
 });
 
 // routes below get items by category
 router.get("/all-speakers", async (req, res) => {
   try {
-    const speakers = await Product.find({ category: "speakers" });
-    res.json({ speakers });
+    const item = await Product.find({ category: "speakers" });
+    res.json({ item });
     console.log(`Found ${speakers.length}`);
   } catch (err) {}
 });
 
 router.get("/all-headphones", async (req, res) => {
   try {
-    const headphones = await Product.find({ category: "headphones" });
-    res.json({ headphones });
-    console.log(`Found ${headphones}`);
+    const item = await Product.find({ category: "headphones" });
+    res.json({ item });
+    console.log(`Found ${item}`);
   } catch (err) {}
 });
 
 router.get("/all-earphones", async (req, res) => {
   try {
-    const earphones = await Product.find({ category: "earphones" });
-    res.json({ earphones });
-    console.log(`Found ${earphones.length} `);
+    const item = await Product.find({ category: "earphones" });
+    res.json({ item });
+    console.log(`Found ${item.length} `);
   } catch (err) {}
 });
 
@@ -51,15 +51,15 @@ router.get("/product/:id", async (req, res) => {
     return;
   }
   try {
-    const products = await Product.findById(id);
+    const item = await Product.findById(id);
 
     if (!id) {
       res.status(404).json({ message: `product not found ${id}` });
       return;
     }
 
-    res.json({ products });
-    console.log("found", products.name);
+    res.json({ item });
+    console.log("found", item.name);
   } catch (err) {
     res.status(500).json(err);
   }
