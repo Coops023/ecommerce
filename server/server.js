3018f87da54dd5a998b1c4155e4815185e4130fd
Coops023 <bjorkc27@gmail.com>
2022-03-01 18:24:58 +0100

    heroku deployemnt

--- server/server.js
+++ server/server.js
@@ -3,16 +3,6 @@ const app = require("./app");
 // ℹ️ Sets the PORT for our app to have access to it. If no env has been set, we hard code it to 3000
 const PORT = process.env.PORT || 5005;
 
-// Accessing the path module
-const path = require("path");
-
-// Step 1:
-app.use(express.static(path.resolve(__dirname, "./client/build")));
-// Step 2:
-app.get("*", function (request, response) {
-  response.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
-});
-
 app.listen(PORT, () => {
   console.log(`Server listening on port http://localhost:${PORT}`);
 });
